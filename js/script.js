7ac3153572309c4cde3bd50eb5bac17fe4849c64
Gabri1391 <chiarellogabriele@gmail.com>
2022-07-11 00:16:04 +0200

    MILESTONE 3 DONE

--- js/script.js
+++ js/script.js
@@ -1,79 +1,115 @@
 // - Recupero gli elementi DOM
-const grid = document.getElementById('grid');
 const button = document.getElementById('button');
 const pointsCounter = document.getElementById('points-counter');
 
-
-//MILESTONE 2
-//Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
-
 // FUNZIONI
-const  createCell = content =>{
-    const cell = document.createElement('div');
-    cell.innerText = content;
-    cell.className = 'cell';
-    grid.appendChild(cell);
+/**
+ * 
+ * @param {Node} cell la cella cliccata
+ * @param {Number[]} bombs l'array contenente le bombe
+ * @returns{boolean} se è game over (true) oppure false
+ */
+function checkGameOver(cell, bombs){
+    const cellNumber = parseInt(cell.innerText);
+    //controllo se ha beccato una bomba
+    if(bombs.includes (cellNumber)){
+        cell.classList.add('bomb');
+        console.log('Hai perso! Hai preso una bomba!')
+        return true;
+    }else{
+        cell.classList.add('safe');
+        console.log('Ottimo! Continua così!')
+        return false;
+    }
 
-    return cell;
 }
 
 //Generiamo 16 numeri random compresi tra 1 e il numero massimo delle celle
-let min = 1;
-let max= 100;
+function createBombs(totalBombs, totalCells){
+    const bombs = [];
+    while(bombs.length < totalBombs){
+        let randomNumber;
+        do{
+            randomNumber = Math.floor(Math.random() * totalCells) + 1;
 
+        }while (bombs.includes(randomNumber));
 
-function generateRandomNumber (min,max) {
-   const randomNumber = Math.floor(Math.random() * (max +1 - min) + min);
-  return randomNumber
+        bombs.push(randomNumber);
+    }
+
+    return bombs;
 }
 
-const bombNumbers = [];
+function play (){
+    
+    //operazioni iniziali
+    this.innerText = 'Ricomincia!';
+    const grid = document.getElementById('grid');
+    grid.classList.remove('none');
+    grid.classList.add('active');  
+    
+    //Svuoto la gliglia a ogni click
+    grid.innerHTML = '';
+    
+    //fase preparatoria
+    let userPoints = 0;
+    const totalBombs = 16;
+    //Punteggio finale
+   
 
-while (bombNumbers.length < 16) {
-   let number = generateRandomNumber(min,max)
-   if(!bombNumbers.includes(number)) {
-    bombNumbers.push(number);
+    //- Impostazioni della griglia
+    const rows = 10;
+    const cells = 10;
+    const totalCells = rows * cells;
+
+    function createCell (cellNumber) {
+    
+        const cell = document.createElement('div');
+        cell.className = 'cell';
+        cell.innerText = cellNumber;
+
+        return cell;
+    }
+
+   //Effetttivo svolgimento del gioco
+   //Genero le bombe
+   const bombs = createBombs(totalBombs, totalCells);
+   console.log(bombs)
+   
+
+   for(let i = 1; i <= totalCells; i++){
+    
+    //Genero una cella
+    const cell = createCell(i);
+
+    //gestisco il click della cella
+    cell.addEventListener('click',function(){
+        //la cella diventa non ricliccabile
+        if(this.classList.contains('clicked')) return;
+
+        //aggiungo classe click
+        this.classList.add('clicked');
+        console.log(i)
+
+        //check se la casella è una bomba o no
+        const isGameOver = checkGameOver (this, bombs);
+        if(!isGameOver)userPoints++;
+    })
+
+    //appendo la cella alla griglia
+    grid.appendChild(cell);
    }
-}
-
-console.log(bombNumbers)
-
-//- Impostazioni della griglia
-const row = 10;
-const cells = 10;
-const totalCells = row * cells;
-
-let userPoints = 0;
 
 
+};
 
-// -Ciclo for per creare 100 celle all'interno della griglia più
-//incremento del punteggio in base a quante celle clicco
-
-for (i = 1; i <= totalCells; i++) {
-    let newCell = createCell(i);
-
-    newCell.addEventListener('click',function(increment){
-
-        if (newCell.classList.contains('clicked')){
-
-            return;
-        }else{
-            this.classList.add('clicked');
-            console.log("Cell: " + this.innerText);
-        }
-
-        userPoints++;
-        pointsCounter.innerText = `Punteggio: ${userPoints}`;
-    } )
-}
 
 // - Aggiungo l'evento al click del bottone
 
-button.addEventListener('click', function () {
+button.addEventListener('click',play);
 
-    grid.classList.remove('none');
-    grid.classList.add('active');
 
-});
+
+
+
 
